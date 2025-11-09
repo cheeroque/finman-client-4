0f8026a6ee5a98b9cffd0136e2273255063e5dc1
Yury Feshchenko <feshenko@vbankcenter.ru>
2025-11-09 14:25:40 +0300

    Move category create button to header

--- app/pages/categories/index.vue
+++ app/pages/categories/index.vue
@@ -1,29 +1,36 @@
 <script setup lang="ts">
 import { useCategoriesQuery } from '~/composables/queries/categories'
 
+definePageMeta({
+  layout: false,
+})
+
 const { state } = useCategoriesQuery()
 
 const { open: openCategoryModal } = useCategoryModal()
 </script>
 
 <template>
-  <UMain as="main">
-    <UContainer class="grid grid-cols-3 gap-5 py-5">
-      <CategoryCard
-        v-for="category in state.data"
-        :key="category.id"
-        :category
-        @click-edit="openCategoryModal({ category })"
-      />
+  <NuxtLayout name="default">
+    <template #header>
+      <UButton
+        color="secondary"
+        variant="soft"
+        @click="openCategoryModal()"
+      >
+        {{ $ts('createCategory') }}
+      </UButton>
+    </template>
 
-      <div class="col-span-full">
-        <UButton
-          size="xl"
-          @click="openCategoryModal()"
-        >
-          {{ $ts('addCategory') }}
-        </UButton>
-      </div>
-    </UContainer>
-  </UMain>
+    <UMain as="main">
+      <UContainer class="grid grid-cols-3 gap-5 py-5">
+        <CategoryCard
+          v-for="category in state.data"
+          :key="category.id"
+          :category
+          @click-edit="openCategoryModal({ category })"
+        />
+      </UContainer>
+    </UMain>
+  </NuxtLayout>
 </template>
