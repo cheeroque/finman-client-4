bfd68d631d8896a1ca2be1f75f2827e7e7fb14e1
Yury Feshchenko <feshenko@vbankcenter.ru>
2025-11-20 10:34:40 +0300

    Add breadcrumb component

---
+++ app/components/_ui/UiBreadcrumb.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { NuxtLink } from '#components'
+import type { BreadcrumbItem } from '~/components/_ui'
+
+defineProps<{
+  items: BreadcrumbItem[]
+}>()
+</script>
+
+<template>
+  <ul
+    class="
+      flex list-none flex-wrap items-baseline gap-1 text-sm
+      text-(--c-text-muted)
+    "
+  >
+    <li
+      v-for="(item, index) in items"
+      :key="`item-${index}`"
+      class="flex items-baseline gap-1"
+    >
+      <component
+        :is="item.active ? 'span' : NuxtLink"
+        :to="item.to"
+        :class="item.active
+          ? 'text-(--c-primary)'
+          : 'hover:text-(--c-primary) hover:underline'
+        "
+      >
+        <Icon
+          v-if="item.icon"
+          :name="item.icon"
+          class="translate-y-px"
+        />
+
+        {{ item.text }}
+      </component>
+
+      <Icon
+        v-if="index < items.length - 1"
+        name="mingcute:right-line"
+        class="translate-y-px text-xs"
+      />
+    </li>
+  </ul>
+</template>
--- app/components/_ui/index.ts
+++ app/components/_ui/index.ts
@@ -1,4 +1,12 @@
 import type { AcceptableValue } from 'reka-ui'
+import type { NuxtLinkProps } from '#app'
+
+export interface BreadcrumbItem {
+  active?: boolean
+  icon?: string
+  text?: string
+  to?: NuxtLinkProps['to']
+}
 
 export interface SelectOption {
   disabled?: boolean
