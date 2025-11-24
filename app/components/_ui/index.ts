796c981a1bcaf17e6ece312b6af3549702f5275e
Yury Feshchenko <feshenko@vbankcenter.ru>
2025-11-24 12:13:06 +0300

    Extend tanstack table column meta interface

--- app/components/_ui/index.ts
+++ app/components/_ui/index.ts
@@ -1,5 +1,16 @@
 import type { AcceptableValue } from 'reka-ui'
+import type { Cell, Header, RowData } from '@tanstack/vue-table'
+
 import type { NuxtLinkProps } from '#app'
+
+declare module '@tanstack/table-core' {
+  interface ColumnMeta<TData extends RowData, TValue> {
+    class?: {
+      th?: string | ((cell: Header<TData, TValue>) => string)
+      td?: string | ((cell: Cell<TData, TValue>) => string)
+    }
+  }
+}
 
 export interface BreadcrumbItem {
   active?: boolean
