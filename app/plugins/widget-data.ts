55b54f77a0e6ef71e84c6ee6c60945525fd61a72
Yury Feshchenko <ronerycamel@gmail.com>
2026-02-20 09:22:45 +0300

    Skip loading widget data if not logged in

--- app/plugins/widget-data.ts
+++ app/plugins/widget-data.ts
@@ -1,6 +1,12 @@
 // Await for useAsyncData calls used in widgets to avoid hydration errors
 
 export default defineNuxtPlugin(async () => {
+  const authStore = useAuthStore()
+
+  if (!authStore.isLoggedIn) {
+    return
+  }
+
   await Promise.all([
     useCurrentMonthTransactions(),
     useTransactionDates(),
