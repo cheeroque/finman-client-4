c9e10605deb9b6684b764638faf0c3a760b99684
Yury Feshchenko <ronerycamel@gmail.com>
2026-05-26 12:04:36 +0300

    Upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,7 +72,6 @@ export default defineNuxtConfig({
 
   vite: {
     plugins: [
-      // @ts-expect-error vite type mismatch between @nuxt/schema and @tailwindcss/vite
       tailwindcss(),
     ],
   },
